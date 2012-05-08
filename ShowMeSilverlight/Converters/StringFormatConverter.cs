e0443cc323acfda4a5ce55a1ebdf98e55f28e407
Michael L Perry <mperry@mallardsoft.com>
2012-05-08 12:27:08 -0500

    Use value converter in Silverlight for easier porting.

---
+++ ShowMeSilverlight/Converters/StringFormatConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ShowMeSilverlight.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("{0:" + parameter.ToString() + "}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
